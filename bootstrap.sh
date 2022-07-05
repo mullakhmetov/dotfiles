98a6d48d6c5785efa7cb1a9b8e3940f40f3582f5
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2022-07-05 17:44:34 +0300

    fix gitconfig, fix runscript, rename in to bootstrap

---
+++ bootstrap.sh
@@ -0,0 +1,33 @@
+# git
+mv ~/.gitconfig ~/gitconfig.copy 2> /dev/null
+mv ~/.gitignore_global ~/gitignore_global.copy 2> /dev/null
+
+ln git/config ~/.gitconfig
+ln git/ignore ~/.gitignore_global
+
+
+# zsh
+source zsh/install-oh-my-zsh.sh
+
+mv ~/.zsh ~/zsh_copy 2> /dev/null
+mv ~/.zshrc ~/zshrc_copy 2> /dev/null
+
+ln zsh/zshrc ~/.zshrc
+mkdir ~/.zsh
+
+ln zsh/config ~/.zsh/config
+ln zsh/plugins ~/.zsh/plugins
+ln zsh/aliases ~/.zsh/aliases
+ln zsh/functions ~/.zsh/functions
+
+
+# tmux
+mv ~/.tmux.conf ~/tmux.conf.copy 2> /dev/null
+ln tmux/conf ~/.tmux.conf
+
+
+# scripts
+# source runscripts.sh
+
+
+
