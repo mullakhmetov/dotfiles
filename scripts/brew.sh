970a3cf0b9b5ce9341800254dcd238b1cbb6cb42
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2022-07-05 13:46:23 +0300

    update brew apps

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -3,6 +3,10 @@ brew upgrade
 
 # cli utils
 brew install coreutils
+brew install moreutils
+brew install findutils
+brew install gnu-sed --with-default-names
+brew install vim --override-system-vi
 brew install git
 brew install wget
 brew install grep
@@ -10,6 +14,13 @@ brew install httpie
 brew install jq
 brew install difftastic
 brew install qcachegrind
+brew install nmap
+brew install fasd
+brew install tmux
+brew install openssh
+brew install openssl
+brew install redis
+brew install sqlite
 
 # apps
 brew cask install 
