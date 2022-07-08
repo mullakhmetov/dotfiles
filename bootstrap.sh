6b56071c7d12b80f5a73ade3a0419ef15d7bedb0
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2022-07-08 10:35:40 +0300

    fix bootstrap: dotfiles should be symlink to repo

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,20 +12,20 @@ source zsh/install-oh-my-zsh.sh
 mv ~/.zsh ~/zsh_copy 2> /dev/null
 mv ~/.zshrc ~/zshrc_copy 2> /dev/null
 
-ln zsh/zshrc ~/.zshrc
+ln -s zsh/zshrc ~/.zshrc
 mkdir ~/.zsh
 
-ln zsh/config ~/.zsh/config
-ln zsh/plugins ~/.zsh/plugins
-ln zsh/aliases ~/.zsh/aliases
-ln zsh/functions ~/.zsh/functions
+ln -s zsh/config ~/.zsh/config
+ln -s zsh/plugins ~/.zsh/plugins
+ln -s zsh/aliases ~/.zsh/aliases
+ln -s zsh/functions ~/.zsh/functions
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
 
 # tmux
 mv ~/.tmux.conf ~/tmux.conf.copy 2> /dev/null
-ln tmux/conf ~/.tmux.conf
+ln -s tmux/conf ~/.tmux.conf
 
 
 # scripts
