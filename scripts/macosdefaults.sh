2df90ab4fcdcc578777fe27cd3ae9699b9e3254c
Artur Mullakhmetov <mullakhmetov.art@gmail.com>
2022-07-11 11:43:11 +0300

    tune key repeat delay

--- scripts/macosdefaults.sh
+++ scripts/macosdefaults.sh
@@ -36,7 +36,7 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain InitialKeyRepeat -int 25
 
 # Set language and text formats
 defaults write NSGlobalDomain AppleLanguages -array "en-RU" "ru-RU"
